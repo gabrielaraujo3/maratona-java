a66c359feb50ff18ab052a41e83ae7d9b87d82ce
Gabriel <araujog388@gmail.com>
2024-05-19 13:42:45 -0300

    operadores aritmeticos

---
+++ src/araujo3/gabriel/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package araujo3.gabriel.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println(resultado);
+    }
+}
